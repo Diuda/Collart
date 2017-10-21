8c1f720026eb534d8d6452e0c1e210196ff46b6a
Diuda <divyanshu.1996@gmail.com>
2017-10-22 01:07:17 +0530

    registering user fixed

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var schema = require('../models/schema');
+var schema = require('../models/register');
 
 
 
