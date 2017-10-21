43e58dc84d3894a1c7a42a5df6c72b18363ccee3
Diuda <divyanshu.1996@gmail.com>
2017-10-21 20:19:58 +0530

    user getting saved with hashed password

--- models/schema.js
+++ models/schema.js
@@ -11,30 +11,34 @@ var Schema = mongoose.Schema;
  })
 
 
- var profileSchema = new Schema({
- 	name: {type: String, required:true},
- 	username: {type: String, required:true, unique:true},
- 	type: {type: String, required:true},
- 	experience: {type: String, required:true},
- 	profession: {type: String, required: true},
- 	city: {type: String},
+ // var profileSchema = new Schema({
+ // 	name: {type: String, required:true},
+ // 	username: {type: String, required:true, unique:true},
+ // 	type: {type: String, required:true},
+ // 	experience: {type: String, required:true},
+ // 	profession: {type: String, required: true},
+ // 	city: {type: String},
  	
- })
+ // })
 
 var User = mongoose.model('User', userSchema);
-var Profile = mongoose.model('Profile', profileSchema);
+// var Profile = mongoose.model('Profile', profileSchema);
 
-module.exports = {
-	User: User,
-	Profile: Profile
-};
+// module.exports = {
+// 	User: User,
+// 	Profile: Profile
+// };
+
+module.exports = User;
 
 module.exports.registerUser = (newUser, callback)=>{
+	// console.log(newUser);
 	bcrypt.genSalt(10, function(err, salt) {
     bcrypt.hash(newUser.password, salt, function(err, hash) {
     	if(err) throw err;
         newUser.password = hash;
-        newUser.User.save(callback)
+        // console.log(newUser.password)
+        newUser.save(callback)
     });
 });
 }
--- routes/register.js
+++ routes/register.js
@@ -2,20 +2,21 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var schema = require('./config/schema');
+var schema = require('../models/schema');
 
 
 
 router.register = (req,res)=>{
-	var newUser = {
+	var newUser = new schema({
 		"name": req.body.name,
-		"username"; req.body.username,
+		"username": req.body.username,
 		"email": req.body.email,
 		"password": req.body.password,
 		"contact": req.body.contact
-	}
+	});
 
 	schema.registerUser(newUser, (err, user)=>{
+		console.log(user);
 		if(err)
 			res.json({success: false, status:120, msg:'cannot regitser user'})
 		else
