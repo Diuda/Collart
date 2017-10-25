ced5c6daa10cc4c7245d4297bfd587effad0e782
Diuda <divyanshu.1996@gmail.com>
2017-10-26 01:49:25 +0530

    logout functionality working

--- angular-stuff/src/app/navbar/navbar.component.ts
+++ angular-stuff/src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../services/auth.service';
+import  {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  	private AuthService:AuthService,
+  	private router: Router
+  	) { }
 
   ngOnInit() {
+  }
+
+    onLogout(){
+    this.AuthService.removeUserData()
+    this.router.navigate(['/login'])
   }
 
 }
