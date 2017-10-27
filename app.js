596de1ed772c902d51c0ffb9462dbaac97605048
Diuda <divyanshu.1996@gmail.com>
2017-10-27 21:41:53 +0530

    modified jwt token

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.post('/authenticate', (req, res, next)=>{
 			});
 			res.json({
 				success:true,
-				token: 'JWT '+token,
+				token: "Bearer "+token,
 				user: {
 					id: user._id,
 					name: user.name,
