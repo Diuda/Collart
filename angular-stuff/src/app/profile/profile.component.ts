e3e874b19837b4d4d3c10b8414d725d55417bec2
Diuda <divyanshu.1996@gmail.com>
2017-10-27 21:40:35 +0530

    added user profile component

--- angular-stuff/src/app/profile/profile.component.ts
+++ angular-stuff/src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../services/validate.service';
+import {AuthService} from '../services/auth.service';
+import  {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+	user: Object;
+
+  constructor(
+  	private authService: AuthService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+
+  	this.authService.getProfile().subscribe(profile => {
+  		this.user = profile.user;
+  	},
+  	err=>{
+  		console.log(err);
+  		return false;
+  	})
   }
+
+
 
 }
