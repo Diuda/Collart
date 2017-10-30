0878cc75fc0d738f2d7c2f9d29e3a35fc787a83d
Diuda <divyanshu.1996@gmail.com>
2017-10-30 16:22:59 +0530

    dont know what happened here

--- angular-stuff/src/app/services/fetchdata.service.ts
+++ angular-stuff/src/app/services/fetchdata.service.ts
@@ -4,12 +4,51 @@ import 'rxjs/add/operator/map'
 
 @Injectable()
 export class FetchdataService {
+	authToken: any;
+	user: any;
+  searchUser: any;
 
   constructor(private http:Http) { }
 
 
   getAllProfile(){
-  	
+  	var headers = new Headers();
+    this.loadToken();
+    // headers.append('Authorization', this.authToken)
+    headers.append('Content-Type', 'application/json')
+    // console.log(user)
+    return this.http.get('http://localhost:3000/users', {headers:headers}).map(res=>res.json());  
   }
+
+  getAllArt(){
+    var headers = new Headers();
+    this.loadToken();
+    // headers.append('Authorization', this.authToken)
+    headers.append('Content-Type', 'application/json')
+    // console.log(user)
+    return this.http.get('http://localhost:3000/art', {headers:headers}).map(res=>res.json());  
+  }
+
+  // setSearchUser(user){
+
+  //   this.searchUser = user;
+  //    console.log(this.searchUser)
+  // }
+
+  getUser(user){
+    var headers = new Headers();
+    this.loadToken();
+    // headers.append('Authorization', this.authToken)
+    headers.append('Content-Type', 'application/json')
+    // console.log(this.searchUser)
+    return this.http.get('http://localhost:3000/profile/'+user, {headers:headers}).map(res=>res.json());      
+  }
+
+    loadToken(){
+    const token = localStorage.getItem('id_token')
+    this.authToken = token
+  }
+
+
 
 }
