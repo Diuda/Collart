95e41935dd1c9a4eee1fd4c52f756e43dcb59d3a
Diuda <divyanshu.1996@gmail.com>
2017-10-30 16:22:20 +0530

    added like function

--- angular-stuff/src/app/services/auth.service.ts
+++ angular-stuff/src/app/services/auth.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/map'
 export class AuthService {
 	authToken: any;
 	user: any;
+  art: any;
 
   constructor(private http: Http) { }
   
@@ -34,11 +35,13 @@ export class AuthService {
   }
 
   storeUserData(token, user){
+    // console.log(JSON.stringify(user.username))
     localStorage.setItem('id_token', token);
-    localStorage.setItem('user', JSON.stringify(user.username));
+    localStorage.setItem('user', user.username);
     this.authToken = token;
     this.user = user;
   }
+
   removeUserData(){
     this.authToken = null;
     this.user = null;
@@ -49,4 +52,38 @@ export class AuthService {
     const token = localStorage.getItem('id_token')
     this.authToken = token
   }
+
+  loadUsername(){
+    const username = localStorage.getItem('user')
+    this.user = username;
+    return this.user;
+  }
+
+    sendArt(art){
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json')
+    console.log(art)
+    return this.http.post('http://localhost:3000/art', art, {headers:headers}).map(res=>res.json()); 
+  }
+
+    getArt(){
+    var headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken)
+    headers.append('Content-Type', 'application/json')
+    // console.log(user)
+    return this.http.get('http://localhost:3000/profile', {headers:headers}).map(res=>res.json());  
+  }
+
+
+  getLikes(like){
+   var headers = new Headers();
+  this.loadToken();
+  // headers.append('Authorization', this.authToken)
+  headers.append('Content-Type', 'application/json')
+  // console.log(user)
+  return this.http.post('http://localhost:3000/likes', like, {headers:headers}).map(res=>res.json());  
+  }
 }
+
+
